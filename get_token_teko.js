d774def6afb07b8bb95655803e45a80f38c575c2
Tùng Phạm Thanh <41067286+th4nhtung@users.noreply.github.com>
2023-03-14 11:40:15 +0700

    Update get_token_teko.js

--- get_token_teko.js
+++ get_token_teko.js
@@ -1,12 +1,14 @@
 // ==UserScript==
 // @name         Get Token TEKO
 // @namespace    http://tampermonkey.net/
-// @version      0.2
+// @version      0.3
 // @description  Get Bearer token on Frontend
 // @author       _-ShiroNeko-_
 // @match        https://*.teko.vn/*
 // @match        https://*.vnpay.vn/*
 // @match        https://*.vnpaytest.vn/*
+// @exclude      https://jira.teko.vn/*
+// @exclude      https://confluence.teko.vn/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=teko.vn
 // @grant        none
 // ==/UserScript==
@@ -15,7 +17,8 @@
     'use strict';
 
     // Your code here...
-    let interval = 1000;
+    let interval = 2000;
+    let fail_count = 0;
     let loading = setInterval(function(){
         if (document.getElementsByTagName("header").length > 0) {
             let token_content_key = null;
@@ -40,6 +43,11 @@
             document.getElementsByTagName("header")[0].appendChild(a);
             clearInterval(loading);
         }
-        interval += 1000;
+        else {
+            fail_count += 1;
+            if (fail_count > 10) {
+                clearInterval(loading);
+            }
+        }
     }, interval);
 })();
